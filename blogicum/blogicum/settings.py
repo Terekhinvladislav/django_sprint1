0512ef14b52c8bc37bee65031a7cda089917162d
Владислав Терехин <terekhinvladislav@gmail.com>
2024-04-13 22:11:45 +0300

    delited all coments 3  version

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -2,15 +2,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
 SECRET_KEY = 'django-insecure-6umi&-wl=#@!&*$k-ph3=yr#6@60j4ujq2)9))8trdn!2899-l'
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
@@ -62,16 +58,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blogicum.wsgi.application'
 
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -87,8 +79,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
 
 LANGUAGE_CODE = 'en-us'
 
